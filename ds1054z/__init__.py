c3c38838d240785269a922345656d258b1b4bf1b
Philipp Klaus <philipp.klaus@gmail.com>
2015-08-06 18:59:54 +0200

    Adding .display_data property to DS1054Z()

--- ds1054z/__init__.py
+++ ds1054z/__init__.py
@@ -1,10 +1,17 @@
 
+import logging
+
 import vxi11
+
+logger = logging.getLogger(__name__)
 
 class DS1054Z(vxi11.Instrument):
 
     ENCODING = 'utf-8'
     H_GRID = 12
+    TMC_HEADER_BYTES = 11
+    TERMINATOR_BYTES = 3
+    DISPLAY_DATA_BYTES = 1152068
 
     def query(self, *args, **kwargs):
         return self.ask(*args, **kwargs)
@@ -21,4 +28,19 @@ class DS1054Z(vxi11.Instrument):
             scal = self.query(":TIMebase:MAIN:SCALe?")
             mdep = self.H_GRID * float(scal) * float(srate)
         return float(mdep)
+
+    @property
+    def display_data(self):
+        # Ask for an oscilloscope display print screen
+        self.write(":DISPlay:DATA?")
+        logger.info("Receiving screen capture...")
+        buff = b""
+        while len(buff) < self.DISPLAY_DATA_BYTES:
+            tmp = self.read_raw(self.DISPLAY_DATA_BYTES)
+            logger.info("read {} bytes in .display_data".format(len(tmp)))
+            if len(tmp) == 0:
+                break
+            buff += tmp
+
+        return buff[self.TMC_HEADER_BYTES:-self.TERMINATOR_BYTES]
 
