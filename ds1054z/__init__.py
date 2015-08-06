37fea33afbf4a0c595b17e3b0a0c96694c3a680a
Philipp Klaus <philipp.klaus@gmail.com>
2015-08-06 18:02:23 +0200

    adding .memory_depth to DS1054Z()

--- ds1054z/__init__.py
+++ ds1054z/__init__.py
@@ -4,6 +4,7 @@ import vxi11
 class DS1054Z(vxi11.Instrument):
 
     ENCODING = 'utf-8'
+    H_GRID = 12
 
     def query(self, *args, **kwargs):
         return self.ask(*args, **kwargs)
@@ -11,4 +12,20 @@ class DS1054Z(vxi11.Instrument):
     def query_raw(self, cmd, *args, **kwargs):
         cmd = cmd.encode(self.ENCODING)
         return self.ask_raw(cmd, *args, **kwargs)
+
+    @property
+    def memory_depth(self):
+        # ACQuire:MDEPth
+        mdep = self.query("ACQ:MDEP?")
+
+        if mdep == "AUTO":
+            # ACQuire:SRATe
+            srate = self.query("ACQ:SRAT?")
+
+            # TIMebase[:MAIN]:SCALe
+            scal = self.query("TIM:SCAL?")
+
+            mdep = self.H_GRID * float(scal) * float(srate)
+
+        return float(mdep)
 
