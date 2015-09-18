524be5957db2c115f1569408d025de356ba78f1c
Philipp Klaus <philipp.klaus@gmail.com>
2015-09-18 23:36:56 +0200

    releasing v0.3.5

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ except (ImportError, IOError, RuntimeError):
     LDESC = ''
 
 setup(name='ds1054z',
-      version = '0.3-dev',
+      version = '0.3.5',
       description = 'Python package and software for the Rigol DS1054Z oscilloscope.',
       long_description = LDESC,
       author = 'Philipp Klaus',
