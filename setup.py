90875fbd0bd1cadd9213c627b7a93ea9c8fff157
Philipp Klaus <philipp.klaus@gmail.com>
2015-09-19 20:37:58 +0200

    setup.py: install_requires universal_usbtmc

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(name='ds1054z',
       include_package_data = True,
       zip_safe = True,
       platforms = 'any',
-      install_requires = ['python_vxi11'],
+      install_requires = ['universal_usbtmc'],
       extras_require = {
           'savescreen':  ["Pillow",],
           'discovery':   ["zeroconf",],
